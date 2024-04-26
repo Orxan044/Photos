fad6558cc66e5980b39a7ae2623e502fa45c851d
Husey_so01 <Husey_so01@STHQ0126-11>
2024-04-26 11:53:49 +0400

    Add project files.

---
+++ Photos/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using DevExpress.Utils.CommonDialogs.Internal;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace Photos;
+
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+
+    private void MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        DirectoryInfo di = new DirectoryInfo(Directory.GetCurrentDirectory());
+        var files = di.GetFiles().ToList();
+        di.GetFiles();
+    }
+}
